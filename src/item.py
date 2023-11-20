9be62d126713028d5b6ef9695a6918b7c63d7cee
nastya shishkina <ana.koze@yandex.ru>
2023-11-20 12:44:03 +0400

    выполнено задание 2

--- src/item.py
+++ src/item.py
@@ -1,10 +1,12 @@
+import csv
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
-    all = []
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+        """
+        Класс для представления товара в магазине.
+        """
+        pay_rate = 1.0
+        all = []
+def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -12,21 +14,39 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+@property
+def name(self):
+    return self.__name
 
-        :return: Общая стоимость товара.
-        """
-        return self.price * self.quantity
+@name.setter
+def name(self, name: str):
+        self.__name = name[:10]
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        self.price *= self.pay_rate
+@classmethod
+def instantiate_from_csv(cls, filename) -> None:
+        Item.all.clear()
+    with open(filename, newline='', encoding='windows-1251') as csvfile:
+            file_reader = csv.DictReader(csvfile, delimiter = ",")
+        for line in file_reader:
+                name = line['name']
+                price = int(line['price'])
+                quantity = int(line['quantity'])
+                cls(name, price, quantity)
+
+@staticmethod
+def string_to_number(num_string: str) -> int:
+    return int(float(num_string))
+
+def calculate_total_price(self) -> float:
+        """Рассчитывает общую стоимость конкретного товара в магазине.
+:return: Общая стоимость товара."""
+    return self.price * self.quantity
+
+def apply_discount(self) -> None:
+        """Применяет установленную скидку для конкретного товара."""
+    self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -10,13 +10,25 @@ def test_item():
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 8000
-
-    item2 = Item("Ноутбук", 20000, 5)
+item2 = Item("Ноутбук", 20000, 5)
     assert item2.name == "Ноутбук"
     assert item2.price == 20000
     assert item2.quantity == 5
     assert item2.calculate_total_price() == 100000
-    Item.pay_rate = 0.8
-    item2.apply_discount()
+Item.pay_rate = 0.8
+item2.apply_discount()
     assert item2.price == 16000
     assert Item.all == [item1, item2]
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    assert Item.all[1].name == 'Ноутбук'
+    assert Item.all[2].price == 10
+    assert Item.all[4].quantity == 5
+
+def test_string_to_number():
+    """Тест для вспомогательного метода tring_to_number"""
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
